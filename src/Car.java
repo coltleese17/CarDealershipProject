5e152f00d00dbaac2bfaafceeb4275767fb20fba
ColtonLeese <coltleese@gmail.com>
2016-11-16 11:13:06 -0500

    added core functionality

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+
+public class Car {
+	private String make;
+	private String model;
+	private int year;
+	private double price;
+	
+	public Car(String make, String model, int year, double price) {
+		
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.price = price;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String makel) {
+		this.make = makel;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
